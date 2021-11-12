c5308fc0959b38664f99af30cc52e891650695a6
Takumi Kawaguchi <tcgds4@gmail.com>
2021-11-12 21:06:36 +0900

    [update]作成ファイル名を指定してHTML取得し保存できるよう修正

--- main.go
+++ main.go
@@ -3,21 +3,46 @@ package main
 import (
 	"encoding/csv"
 	"fmt"
+	"io/ioutil"
 	"log"
-	"strings"
+	"net/http"
+	"os"
 )
 
 func main() {
-	s := `名前,年齢,身長,体重
-Tanaka,31,190cm,97kg
-Suzuki,46,180cm,79kg
-Matsui,45,188cm,95kg
-`
-	r := csv.NewReader(strings.NewReader(s))
-
-	record, err := r.ReadAll()
+	f, err := os.Open("access.csv")
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("%#v\n", record)
+	defer f.Close()
+
+	r := csv.NewReader(f)
+	records, err := r.ReadAll()
+	if err != nil {
+		log.Fatal()
+	}
+
+	for _, record := range records {
+		res, err := http.Get(record[1])
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer res.Body.Close()
+
+		if res.StatusCode != http.StatusOK {
+			log.Fatalf("statuscode: %v\n", res.StatusCode)
+		}
+
+		f, err := os.Create(fmt.Sprintf("output/%s.html", record[0]))
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer f.Close()
+
+		body, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		f.Write(body)
+	}
 }
